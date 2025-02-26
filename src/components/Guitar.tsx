636adb2fda826da771e16cfed8748d709d8ccdbe
acosta913 <acostapaez913@gmail.com>
2025-02-25 22:32:23 -0500

    add types file

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,18 +1,11 @@
-type Guitar = {
-  id: number;
-  name: string;
-  image: string;
-  description: string;
-  price: number;
-};
+import type { Guitar } from "../types/types";
 
-export default function Guitar({
-  guitar,
-  addToCart,
-}: {
+type GuitarProps = {
   guitar: Guitar;
   addToCart: (item: Guitar) => void;
-}) {
+};
+
+export default function Guitar({ guitar, addToCart }: GuitarProps) {
   const { name, image, description, price } = guitar;
 
   //   const handleClick = (guitar) => {
